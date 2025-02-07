d2bacd17e86794f6c281a07fa4e41a8109616bba
Danila P <panagushindmwork@gmail.com>
2025-02-07 19:43:06 +0300

    fix tests

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,21 +1,10 @@
-import os
-
 import pytest
-import asyncio
 
 from fastapi.testclient import TestClient
 
-from database import db
 from main import app
 
 
 @pytest.fixture(scope="session")
 def client():
     return TestClient(app)
-
-
-@pytest.fixture()
-def event_loop():
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
--- src/tests/test_api.py
+++ src/tests/test_api.py
@@ -1,5 +1,6 @@
 import pytest
 
+@pytest.mark.asyncio
 def test_get_wallet(client):
 
     response = client.post("api/wallet", json={"address": "TCQPAt3swdNMadK4DcbzGNnA146D5uQteZ"})
@@ -9,10 +10,3 @@ def test_get_wallet(client):
     assert "balance_trx" in data
     assert "energy" in data
     assert "bandwidth" in data
-
-def test_get_wallet_requests(client):
-
-    response = client.get("api/wallet-requests")
-    assert response.status_code == 200
-    data = response.json()
-    assert isinstance(data["items"], list)
